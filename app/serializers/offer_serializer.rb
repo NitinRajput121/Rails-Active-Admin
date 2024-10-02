109a369c1ce3c2bb23c00ba0a852625680173943
NitinRajput121 <nitinrajput8248@gmail.com>
2024-10-02 19:22:57 +0530

    fifth

--- app/serializers/offer_serializer.rb
+++ app/serializers/offer_serializer.rb
@@ -1,18 +1,13 @@
 class OfferSerializer < ActiveModel::Serializer
-  attributes :id, :offer_name, :discount, :start_date, :end_date, :discounted_price
+  attributes :id, :offer_name, :discount, :start_date, :end_date, :discounted_price, :catalogue_name
    
-  # belongs_to :catalogue_variant, serializer: CatalogueVariantSerializer, if: -> { object.catalogue_variant.present? }
 
 
-  # attribute :catalogue_variant_name do |object|
-  #   object.object.catalogue_variant.name
-  # end
-
-  attribute :catalogue_variant_price do |object|
+  attribute :original_price do |object|
     object.object.catalogue_variant.price
   end
 
-  attribute :catalogue_variant_quantity do |object|
+  attribute :quantity do |object|
     object.object.catalogue_variant.quantity
   end
 
@@ -20,4 +15,21 @@ class OfferSerializer < ActiveModel::Serializer
     object.discounted_price
   end
 
+  def catalogue_name
+    object.catalogue_variant.catalogue.name if object.catalogue_variant.catalogue
+  end
+
+  def catalogue_name
+    object.catalogue_variant.catalogue.name if object.catalogue_variant.catalogue
+  end
+
+
+
 end
+
+  # belongs_to :catalogue_variant, serializer: CatalogueVariantSerializer, if: -> { object.catalogue_variant.present? }
+
+
+  # attribute :catalogue_variant_name do |object|
+  #   object.object.catalogue_variant.name
+  # end
