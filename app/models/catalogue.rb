b2903c84b0e040ba5c57ad19d8ebd0c8bbb152cc
NitinRajput121 <nitinrajput8248@gmail.com>
2024-10-05 20:22:00 +0530

    changes saved

--- app/models/catalogue.rb
+++ app/models/catalogue.rb
@@ -1,21 +1,21 @@
-# class Catalogue < ApplicationRecord
-# 	has_many :catalogue_variants, dependent: :destroy
-# 	has_many :caart_items
-#     has_many :wishlists
-# 	belongs_to :category
-# 	belongs_to :sub_category
+class Catalogue < ApplicationRecord
+	has_many :catalogue_variants, dependent: :destroy
+	has_many :caart_items
+    has_many :wishlists
+	belongs_to :category
+	belongs_to :sub_category
 
-#     enum gender: { male: 0, female: 1, unisex: 2 }
+    enum gender: { male: 0, female: 1, unisex: 2 }
 
 
-# 	accepts_nested_attributes_for :catalogue_variants, allow_destroy: true
+	accepts_nested_attributes_for :catalogue_variants, allow_destroy: true
 
-#     scope :filter_by_gender, -> (gender) { where(gender: gender)}
-# 	scope :search, ->(q) { where("name LIKE ? OR description LIKE ?", "%#{q}%", "%#{q}%") }
-#     scope :sorted_by, ->(sorted_by) { order(sorted_by) }
+    scope :filter_by_gender, -> (gender) { where(gender: gender)}
+	scope :search, ->(q) { where("name LIKE ? OR description LIKE ?", "%#{q}%", "%#{q}%") }
+    scope :sorted_by, ->(sorted_by) { order(sorted_by) }
 
   
-# 	validates :name, presence: true
-# 	validates :category, presence: true
-# 	validates :sub_category, presence: true
-# end
+	validates :name, presence: true
+	validates :category, presence: true
+	validates :sub_category, presence: true
+end
